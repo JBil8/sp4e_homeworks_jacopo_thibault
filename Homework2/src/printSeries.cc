f6f0dbca5132e79d1824ef8d5e29b0be61787ec0
Thibault Ghesquiere <ghesquie@lsmspc19.epfl>
2023-10-31 13:24:35 +0100

    Added convergence toward analytic prediction in the dump function of PrintSeries class.

--- Homework2/src/printSeries.cc
+++ Homework2/src/printSeries.cc
@@ -1,5 +1,6 @@
 #include "printSeries.hh"
 #include <iostream>
+#include <cmath>
 
 PrintSeries::PrintSeries(Series & series, int frequency, int maxiter)
     : DumperSeries(series),  // Call the constructor of DumperSeries
@@ -7,10 +8,24 @@ PrintSeries::PrintSeries(Series & series, int frequency, int maxiter)
       maxiter(maxiter) {};
 
 void PrintSeries::dump() {
-    // std::cout << "Series dump:" << std::endl;
     std::cout << "Frequency: " << frequency << std::endl;
     std::cout << "Max iterations: " << maxiter << std::endl;
 
+    double analytic_prediction = series.getAnalyticPrediction();
+    
+    /* if (!std::isnan(analytic_prediction)) {
+        std::cout << "Analytic prediction: " << analytic_prediction << std::endl;
+    } else {
+        std::cout << "Analytic prediction is not available." << std::endl;
+    } */
+
     for (int i = 1; i < maxiter+1; i += frequency) {
-        std::cout << "Series value: " << series.compute(i) << std::endl;}
+        double series_value = series.compute(i);
+        std::cout << "Series value at iteration " << i << ": " << series_value;
+
+        if (!std::isnan(analytic_prediction)) {
+            double convergence = std::abs(series_value - analytic_prediction);
+            std::cout << " | Convergence: " << convergence << std::endl;
+        }
+    }
 };
