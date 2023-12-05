6c485ae051608b367a4357475163b0eb6e491028
Thibault Ghesquiere <ghesquie@lsmspc19.epfl>
2023-12-05 15:39:02 +0100

    Corrected and simplified the Matrix::transform function

--- Homework3/src/fft.hh
+++ Homework3/src/fft.hh
@@ -17,30 +17,18 @@ struct FFT {
 /* ------------------------------------------------------ */
 
 inline Matrix<complex> FFT::transform(Matrix<complex>& m_in) {
-  UInt rows = m_in.rows();
-  UInt cols = m_in.cols();
+  
+  int N = m_in.size();
+  Matrix <complex> m_out(N);
 
-  // Allocate FFTW input and output arrays
-  fftw_complex *in = reinterpret_cast<fftw_complex*>(m_in.data());
-  fftw_complex *out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * rows * cols);
-
-  // Create and execute the plan
-  fftw_plan plan = fftw_plan_dft_2d(rows, cols, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+  fftw_plan plan = fftw_plan_dft_2d(N, N,
+                                    reinterpret_cast<fftw_complex*>(m_in.data()),
+                                    reinterpret_cast<fftw_complex*>(m_out.data()),
+                                    FFTW_FORWARD, FFTW_ESTIMATE);
+  
   fftw_execute(plan);
 
-  // Create result Matrix and copy FFTW output to it
-  Matrix<complex> m_out(m_in.size());
-  for (UInt i = 0; i < rows; ++i) {
-    for (UInt j = 0; j < cols; ++j) {
-      // Copying real and imaginary parts individually
-      m_out(i, j) = complex(out[i * cols + j][0], out[i * cols + j][1]);
-    }
-  }
-  //std::copy(out, out + rows * cols, reinterpret_cast<fftw_complex*>(m_out.data()));
-
-  // Free memory
   fftw_destroy_plan(plan);
-  fftw_free(out);
 
   return m_out;
 }
