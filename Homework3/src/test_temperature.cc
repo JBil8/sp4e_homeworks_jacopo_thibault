a9ff2809a5201d4b922f79095018f3a85e85a57c
JBil8 <jacopo.bilotto@epfl.ch>
2023-12-11 15:08:09 +0100

    Homogenous test passed

--- Homework3/src/test_temperature.cc
+++ Homework3/src/test_temperature.cc
@@ -4,6 +4,7 @@
 #include "matrix.hh"
 #include "material_point.hh"
 #include "my_types.hh"
+#include <cmath>
 #include <gtest/gtest.h>
 
 /*****************************************************************/
@@ -44,6 +45,7 @@ TEST_F(InitialConditions, homogeneous) {
 
     for (UInt i = 0; i < N; ++i) {
         MaterialPoint &p = dynamic_cast<MaterialPoint&>(system.getParticle(i));
+        p.getTemperature() = 0.0;
     }
 
     Matrix<complex> heat_source(N);
@@ -53,5 +55,47 @@ TEST_F(InitialConditions, homogeneous) {
         }
 
     temperature = std::make_shared<ComputeTemperature>(dt, 1.0, 1.0, 1.0, heat_source);
+    temperature->compute(system);
+    for (UInt i = 0; i < N; ++i) {
+        MaterialPoint &p = dynamic_cast<MaterialPoint&>(system.getParticle(i));
+        ASSERT_NEAR(0.0, p.getTemperature(), 1e-10);
+    }
+}
 
+TEST_F(InitialConditions, volumetric_heat_source) {
+
+    Real x_min, x_max, y_min, y_max;
+    x_min = -1.0;
+    x_max = 1.0;
+    y_min = -1.0;
+    y_max = 1.0;
+
+    for (UInt i = 0; i < sqrt(N); ++i) {
+        for (UInt j = 0; j < sqrt(N); ++j) {
+            MaterialPoint &p = dynamic_cast<MaterialPoint&>(system.getParticle(i*sqrt(N) + j));
+            p.getPosition()[0] = x_min + (x_max - x_min) / sqrt(N) * i;
+            //p.getPosition()[1] = y_min + (y_max - y_min) / sqrt(N) * j;
+            p.getTemperature() = sin(2*M_PI*p.getPosition()[0]/(x_max - x_min));
+        }
+    }
+
+    Matrix<complex> heat_source(N);
+    Real scalar_factor = pow(2*M_PI/(x_max-x_min), 2) ;
+    for (auto&& entry : index(heat_source)) {
+        //assign value of sin similar to initial temperature distribution defined above
+        auto& i = std::get<0>(entry);
+        auto& j = std::get<1>(entry);
+        auto& val = std::get<2>(entry);
+        val = scalar_factor* sin(2*M_PI*i/(x_max-x_min));
+        }
+
+    temperature = std::make_shared<ComputeTemperature>(dt, 1.0, 1.0, 1.0, heat_source);
+    temperature->compute(system);
+
+    for (UInt i = 0; i < sqrt(N); ++i) {
+        for (UInt j = 0; j < sqrt(N); ++j) {
+            MaterialPoint &p = dynamic_cast<MaterialPoint&>(system.getParticle(i*sqrt(N) + j));
+            ASSERT_NEAR(p.getTemperature(), sin(2*M_PI*p.getPosition()[0]/(x_max - x_min)), 1e-5);
+        }
+    }
 }
