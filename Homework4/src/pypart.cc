ee73aa66b973fae9fe724ec250d08eb194788e0f
Thibault Ghesquiere <ghesquie@lsmspc19.epfl>
2024-01-03 11:05:16 +0100

    Started the ParticleFactoryInterface binding

--- Homework4/src/pypart.cc
+++ Homework4/src/pypart.cc
@@ -12,5 +12,33 @@ PYBIND11_MODULE(pypart, m) {
 
   m.doc() = "pybind of the Particles project";
 
-  // bind the routines here
+  // ParticlesFactoryInterface
+  py::class_<ParticlesFactoryInterface>(
+            m, "ParticlesFactoryInterface",
+            py::dynamic_attr() // to allow new members to be created dynamically
+    )
+    
+    .def("createSimulation", 
+            [](ParticlesFactoryInterface &self, const std::string &fname, Real timestep) -> SystemEvolution& {
+                return self.createSimulation(fname, timestep);
+            },
+            py::return_value_policy::reference_internal, // Keeps the object alive as long as the parent object is alive
+            py::arg("fname"), py::arg("timestep")
+        )
+
+    .def("createSimulation", 
+            [](ParticlesFactoryInterface &self, const std::string &fname, Real timestep, py::function func) -> SystemEvolution& {
+                auto wrapper = [func](Real timestep) { func(timestep); };
+                return self.createSimulation(fname, timestep, wrapper);
+            },
+            py::return_value_policy::reference_internal, // Keeps the object alive as long as the parent object is alive
+            py::arg("fname"), py::arg("timestep"), py::arg("func")
+        )
+
+  // PlanetsFactory
+
+  // MaterialPointsFactory
+
+  // PingPongBallsFactory
+
 }
